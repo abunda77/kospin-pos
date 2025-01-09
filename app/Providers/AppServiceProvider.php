f15df462dbdb157a576476514bef3efa2e2d7987
Erie Putranto <erieputranto@gmail.com>
2025-01-09 20:25:21 +0700

    update: appservice provide 3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(config('app.env') === 'production') {
+        if (env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS', true);
         }
 
         Scramble::afterOpenApiGenerated(function (OpenApi $openApi) {
