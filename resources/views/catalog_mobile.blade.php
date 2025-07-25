7e6517049966a18137d95fd195a6300380ce480c
Erie Putranto <erieputranto@gmail.com>
2025-07-25 19:24:10 +0700

    fix : card kategori Semua

--- resources/views/catalog_mobile.blade.php
+++ resources/views/catalog_mobile.blade.php
@@ -31,7 +31,7 @@
     <div class="flex overflow-x-auto snap-x pb-4 -mx-2 hide-scrollbar">
         <div class="flex-shrink-0 w-36 mx-2 snap-start">
             <a href="{{ route('catalog.mobile') }}"
-               class="flex gap-3 items-center px-4 py-3 w-full rounded-xl shadow transition-all duration-300 hover:bg-primary-50 hover:shadow-md group {{ !request()->category ? 'ring-2 ring-primary-500 bg-primary-50 bg-gray-100' : 'bg-gray-100' }}">
+               class="flex gap-3 items-center px-4 py-3 w-full rounded-xl shadow transition-all duration-300 hover:bg-primary-50 hover:shadow-md group {{ !request()->category ? 'bg-gray-200' : 'bg-gray-100' }}">
                 <div class="flex justify-center items-center min-w-[3rem] h-12 bg-gradient-to-br rounded-lg from-primary-50 to-primary-100">
                     <svg class="w-6 h-6 text-primary-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"/>
